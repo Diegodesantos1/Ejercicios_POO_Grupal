7a34c2cccc583759c038dbeea0a16b949ea3d5aa
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-03-18 15:31:40 +0100

    creo que he conseguido instanciar parte del código, pero todavía tiene muchos fallos

--- Clases/herencia.py
+++ Clases/herencia.py
@@ -1,28 +1,28 @@
-
 class Pared:
-    pared=int(input("¿Cuántas paredes tiene la casa?"))
     def __init__(self,pared):
         self.pared=pared
 
 class Ventana:
-    ventana=int(input("¿Cuántas ventanas tiene la casa?"))
-    def __init__(self,ventana):
+    def __init__(self,ventana,num_ventana):
         self.ventana=ventana
+        self.num_ventana=num_ventana
 
 class Casa:
-    def __init__(self,casa):
+    def __init__(self,casa,espacio):
         self.casa=casa
-class Superficie:
-    def calcular():
+        self.espacio=espacio
+    def definir(espacio):
         pared_norte = Pared("NORTE")
         pared_oeste = Pared("OESTE")
         pared_sur = Pared("SUR")
         pared_este = Pared("ESTE")
+        espacio=[pared_norte, pared_oeste, pared_sur, pared_este]
         ventana_norte = Ventana(pared_norte, 0.5)
         ventana_oeste = Ventana(pared_oeste, 1)
         ventana_sur = Ventana(pared_sur, 2)
         ventana_este = Ventana(pared_este, 1)
-        casa = Casa([pared_norte, pared_oeste, pared_sur, pared_este])
-        print(casa.superficie_acristalada())
+    def superficie_acristalada(espacio):
+        casa = Casa(espacio)
+        print(casa.superficie_acristalada(espacio))
+Casa.superficie_acristalada(espacio)
 
-Superficie.calcular()

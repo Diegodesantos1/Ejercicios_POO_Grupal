c337f07e023a086565b8b8da2ae994a5534e7e05
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-03-18 15:04:15 +0100

    cambios en las clases del ejercicio de herencia

---
+++ Clases/herencia.py
@@ -0,0 +1,28 @@
+
+class Pared:
+    pared=int("¿Cuántas paredes tiene la casa?")
+    def __init__(self,pared):
+        self.pared=pared
+
+class Ventana:
+    ventana=int("¿Cuántas ventanas tiene la casa?")
+    def __init__(self,ventana):
+        self.ventana=ventana
+
+class Casa:
+    def __init__(self,casa):
+        self.casa=casa
+class Superficie:
+    def calcular():
+        pared_norte = Pared("NORTE")
+        pared_oeste = Pared("OESTE")
+        pared_sur = Pared("SUR")
+        pared_este = Pared("ESTE")
+        ventana_norte = Ventana(pared_norte, 0.5)
+        ventana_oeste = Ventana(pared_oeste, 1)
+        ventana_sur = Ventana(pared_sur, 2)
+        ventana_este = Ventana(pared_este, 1)
+        casa = Casa([pared_norte, pared_oeste, pared_sur, pared_este])
+        print(casa.superficie_acristalada())
+
+Superficie.calcular()

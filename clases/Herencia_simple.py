dfd47706d4fc82b74e0c1db8646b958f503378d1
Xavitheforce <javier.miguelez.yague@gmail.com>
2022-03-20 13:25:51 +0100

    Update Herencia_simple.py

--- clases/Herencia_simple.py
+++ clases/Herencia_simple.py
@@ -2,18 +2,40 @@ class Punto2D:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.mostrar(self.x, self.y)
-        
 
-    def mostrar(self, a, b):
-        return "X: "+str(a), "Y: "+str(b)
-
+    def mostrar(self):
+        return "X: "+str(self.x)+"; Y: "+str(self.y)
 
     def traslacion(self, a, b):
         self.x += a
         self.y += b
         return self.x, self.y
 
-a = Punto2D(3, 6)
-print("A = {}".format(a))
+class Punto3D:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def mostrar(self):
+        return "X: "+str(self.x)+"; Y: "+str(self.y)+"; Z: "+str(self.z)
+
+    def traslacion(self, a, b, c):
+        self.x += a
+        self.y += b
+        self.z += c
+        return self.x, self.y, self.z
+
+a = Punto2D(1, 2) 
+print("A = {}".format(a)) 
  
+a.traslacion(-1, -2) 
+print("A = {}".format(a)) 
+ 
+b = Punto2D(-3, 0) 
+b.traslacion(5, -1) 
+print("B = {}".format(b))
+
+c = Punto3D(1,5,-3) 
+c.traslacion(0, -2, 1) 
+print("C = {}".format(c)) 

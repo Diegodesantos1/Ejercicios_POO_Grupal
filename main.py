6de41cd7211901aa3c743b25d6fe75aac6362375
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-03-21 15:29:37 +0100

    añado una función

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 #main
-variable = int(input("Conceptos de la POO en Python. Ejercicios de Herencias. /nPor favor, introduzca qué ejercicio desea realizar (1, 2 o 3): "))
+def elegir():
+    variable = int(input("Conceptos de la POO en Python. Ejercicios de Herencias. /nPor favor, introduzca qué ejercicio desea realizar (1, 2 o 3): "))
 
-if variable == 1:
-    from Clases import herencia_multiple_diamante
-elif variable == 2:
-    from Clases import herencia_simple
-elif variable == 3:
-    from Clases import herencia
-else:
-    print("Sólo son válidos los valores 1, 2 y 3. ")
+    if variable == 1:
+        from Clases import herencia_multiple_diamante
+    elif variable == 2:
+        from Clases import herencia_simple
+    elif variable == 3:
+        from Clases import herencia
+    else:
+        print("Sólo son válidos los valores 1, 2 y 3. ")
+elegir()
 
 if __name__ == "main":
     main()

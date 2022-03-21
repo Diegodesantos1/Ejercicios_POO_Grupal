f3bb40c34aecd370391a1a8f0e58e8a51e921abd
Xavitheforce <javier.miguelez.yague@gmail.com>
2022-03-21 16:39:06 +0100

    .

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 
 def elegir():
-    print("\nSi desea ver la salida estándar del ejercicio 2 acceda a la carpeta imágenes. ")
+    print("\nSi desea ver la resolución del ejercicio 2 acceda a la carpeta imágenes. ")
     variable = int(input("\nConceptos de la POO en Python. Ejercicios de Herencias. \n\nPor favor, introduzca qué ejercicio desea realizar: \n --> 1: Herencia simple\n --> 3: Herencia múltiple (diamante)\n --> 4: Herencia múltiple (casa)\n"))
     if variable == 1:
-        from Clases import herencia_simple
+        from clases import herencia_simple
     elif variable == 3:
-        from Clases import herencia_multiple_diamante
+        from clases import herencia_multiple_diamante
     elif variable == 4:
-        from Clases import herencia
+        from clases import herencia
     else:
         print("Sólo son válidos los valores 1,3 y 4.\n")
         elegir()
 elegir()
-
-

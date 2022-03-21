992ed514c6515f4c357804719011a85d2750c67f
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-03-21 15:38:02 +0100

    perdón había puesto mal los ejercicios

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 #main
 def elegir():
-    variable = int(input("Conceptos de la POO en Python. Ejercicios de Herencias. /nPor favor, introduzca qué ejercicio desea realizar (1, 2 o 3): "))
+    variable = int(input("Conceptos de la POO en Python. Ejercicios de Herencias. /nPor favor, introduzca qué ejercicio desea realizar (1, 2, 3 o 4): "))
 
     if variable == 1:
-        from Clases import herencia_multiple_diamante
-    elif variable == 2:
         from Clases import herencia_simple
+    if variable == 2:
+        from imagenes import salidaestandar
+    elif variable == 3:
+        from Clases import herencia_multiple_diamantes
     elif variable == 3:
         from Clases import herencia
     else:

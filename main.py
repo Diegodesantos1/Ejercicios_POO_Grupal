d735afd8cd5dc23e709fc0ee73a7921bd4039b6b
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-03-21 16:35:42 +0100

    main arreglado temporalmente

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 
 def elegir():
-    variable = int(input("\nConceptos de la POO en Python. Ejercicios de Herencias. \nPor favor, introduzca qué ejercicio desea realizar: \n --> 1: Herencia simple\n --> 2: Salida éstandar\n --> 3: Herencia múltiple (diamante)\n --> 4: Herencia múltiple (casa)\n"))
+    print("\nSi desea ver la resolución del ejercicio 2 acceda a la carpeta imágenes. ")
+    variable = int(input("\nConceptos de la POO en Python. Ejercicios de Herencias. \n\nPor favor, introduzca qué ejercicio desea realizar: \n --> 1: Herencia simple\n --> 3: Herencia múltiple (diamante)\n --> 4: Herencia múltiple (casa)\n"))
     if variable == 1:
         from Clases import herencia_simple
-    elif variable == 2:
-        from imagenes import salidaestandar
     elif variable == 3:
         from Clases import herencia_multiple_diamante
     elif variable == 4:
         from Clases import herencia
     else:
-        print("Sólo son válidos los valores 1, 2, 3 y 4.\n")
+        print("Sólo son válidos los valores 1,3 y 4.\n")
         elegir()
 elegir()
 
-if __name__ == "main":
-    main()
 

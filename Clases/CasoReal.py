9df498e120001758563a3114c88416ad63b3323b
Matota <72810000+mat0ta@users.noreply.github.com>
2022-03-21 16:50:17 +0000

    Terminado 2º parte 4

--- Clases/CasoReal.py
+++ Clases/CasoReal.py
@@ -1,30 +1,38 @@
+casa = {}
+orientaciones = ['NORTE', 'SUR', 'ESTE', 'OESTE']
+
 class Casa():
-    def __init__(self):
-      self.casa = {}
-    def Paredes(self, orientacion):
-      for i in range(len(orientacion)):
-        nombre = orientacion[i]
-        self.casa[nombre] = {
-            'ventanas': {},
-        }
-      print(self.casa)
-      Casa().Ventanas(self.casa, [['NORTE', 0.5], ['SUR', 1], ['ESTE', 2], ['OESTE', 1]])
-    def Ventanas(self, casa, ventanas):
-      dimensiones = []
-      for i in range(len(ventanas)):
-        nombre = ventanas[i][0]
-        casa[nombre]['ventanas'] = {
-          1: ventanas[i][1]
-        }
-        dimensiones.append(ventanas[i][1])  
-      print(casa)
-      Casa().Superficie(dimensiones)
-      
-    def Superficie(self, dimensiones):
-      total = 0
-      for i in range(len(dimensiones)):
-        total += dimensiones[i]
-      print('Superficie avristalada: ' + str(total))
+  global casa
+  def Paredes(self, orientacion):
+    for i in range(len(orientacion)):
+      nombre = orientacion[i]
+      casa[nombre] = {
+          'ventanas': {},
+      }
+    print(casa)
+    Casa().Ventanas([['NORTE', 0.5], ['SUR', 1], ['ESTE', 2], ['OESTE', 1]])
+  def Ventanas(self, ventanas):
+    dimensiones = []
+    for i in range(len(ventanas)):
+      nombre = ventanas[i][0]
+      casa[nombre]['ventanas'] = {
+        'superficie': ventanas[i][1]
+      }
+      dimensiones.append(ventanas[i][1])  
+    print(casa)
+    Casa().Superficie()
+  def Superficie(self):
+    total = 0
+    for i in range(len(orientaciones)):
+      total += casa[orientaciones[i]]['ventanas']['superficie']
+    print('Superficie avristalada: ' + str(total))
+  def ParedCortina(self, orientacion, tamaño):
+    casa[orientacion]['ventanas']['superficie'] += tamaño
+    print(casa)
    
   
 # Casa().Paredes(['NORTE', 'SUR', 'ESTE', 'OESTE']) <-- Esto pal main
+
+Casa().Paredes(['NORTE', 'SUR', 'ESTE', 'OESTE'])
+Casa().ParedCortina('NORTE', 4)
+Casa().Superficie()

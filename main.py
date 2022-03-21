e64a977f4563cf42685e1f63fb8929dfc05326f6
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-03-21 16:28:53 +0100

    mejorado el readme

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 
 def elegir():
-    variable = int(input("Conceptos de la POO en Python. Ejercicios de Herencias. \nPor favor, introduzca qué ejercicio desea realizar (1, 2, 3 o 4): "))
-
+    variable = int(input("\nConceptos de la POO en Python. Ejercicios de Herencias. \nPor favor, introduzca qué ejercicio desea realizar: \n --> 1: Herencia simple\n --> 2: Salida éstandar\n --> 3: Herencia múltiple (diamante)\n --> 4: Herencia múltiple (casa)\n"))
     if variable == 1:
         from Clases import herencia_simple
     elif variable == 2:
         from imagenes import salidaestandar
     elif variable == 3:
-        from Clases import herencia_multiple_diamantes
+        from Clases import herencia_multiple_diamante
     elif variable == 4:
         from Clases import herencia
     else:
-        print("Sólo son válidos los valores 1, 2, 3 y 4. ")
+        print("Sólo son válidos los valores 1, 2, 3 y 4.\n")
+        elegir()
 elegir()
 
 if __name__ == "main":

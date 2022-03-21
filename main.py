6cf7dde9d27ce712c65b6838de6ee8e3c3ecb615
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-03-21 15:22:28 +0100

    ej 2 y 3

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ variable = int(input("Conceptos de la POO en Python. Ejercicios de Herencias. /n
 
 if variable == 1:
     from Clases import herencia_multiple_diamante
+elif variable == 2:
+    from Clases import herencia_simple
+elif variable == 3:
+    from Clases import herencia
+else:
+    print("Sólo son válidos los valores 1, 2 y 3. ")
 
 if __name__ == "main":
-
+    

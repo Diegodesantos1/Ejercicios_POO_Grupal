7ede8150f57f6cfd96d60bc9a619d6229b9ed24c
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-03-21 17:45:18 +0100

    añado el 4 al main

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
+from Clases.CasoReal import *
 
 def elegir():
     print("\nSi desea ver la resolución del ejercicio 2 acceda a la carpeta imágenes. ")
     variable = int(input("\nConceptos de la POO en Python. Ejercicios de Herencias. \n\nPor favor, introduzca qué ejercicio desea realizar: \n --> 1: Herencia simple\n --> 3: Herencia múltiple (diamante)\n --> 4: Herencia múltiple (casa)\n"))
     if variable == 1:
-        from clases import herencia_simple
+        from Clases import herencia_simple
     elif variable == 3:
-        from clases import herencia_multiple_diamante
+        from Clases import herencia_multiple_diamante
     elif variable == 4:
-        from clases import herencia
+        Casa().Paredes(['NORTE', 'SUR', 'ESTE', 'OESTE'])
     else:
         print("Sólo son válidos los valores 1,3 y 4.\n")
         elegir()

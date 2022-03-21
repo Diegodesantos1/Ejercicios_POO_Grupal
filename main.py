e5e3364afcc3ceec6f3d0a285bc5cf249f7c7aad
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-03-21 15:19:34 +0100

    ejer 1

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 #main
-variable = int(input("Conceptos de la POO en Python. Ejercicios de Herencias. /nPor favor, introduzca qué ejercicio desea realizar: "))
+variable = int(input("Conceptos de la POO en Python. Ejercicios de Herencias. /nPor favor, introduzca qué ejercicio desea realizar (1, 2 o 3): "))
+
+if variable == 1:
+    from Clases import herencia_multiple_diamante
+
 if __name__ == "main":
 
